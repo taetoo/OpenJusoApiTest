8649cf2e44da7355dab1e2f69aec7513d5903b44
taetoo <rlaougs5@naver.com>
2022-09-05 16:33:46 +0900

    jibun + h_code 완성

--- src/main/java/com/example/jusoapitest/JusoApi.java
+++ src/main/java/com/example/jusoapitest/JusoApi.java
@@ -1,6 +1,7 @@
 package com.example.jusoapitest;
 
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
@@ -16,12 +17,11 @@ public class JusoApi {
 
         // 파싱한 데이터를 저장할 변수
         String result = "";
-        String confmKey = "devU01TX0FVVEgyMDIyMDkwMjE2MTYzOTExMjk0NDM=";
 
         try {
             // 인증키 (개인이 받아와야함)
 
-            String keyword = URLEncoder.encode("도곡로 218", "UTF-8");
+            String keyword = URLEncoder.encode("구로동 97-6", "UTF-8");
 
 
             URL url = new URL("https://business.juso.go.kr/addrlink/addrLinkApi.do?currentPage=1&countPerPage=10&keyword="
@@ -34,8 +34,33 @@ public class JusoApi {
 
             result = bf.readLine();
 
-            System.out.println(result);
+//            System.out.println(result);
 
+            // String 값을 JSON 형태로 추출하기 위해 사용하는 라이브러리
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObject = (JSONObject)jsonParser.parse(result);
+            JSONObject addResult = (JSONObject)jsonObject.get("results");
+
+            // 리스트 추출
+            JSONArray jusoArray = (JSONArray)addResult.get("juso");
+            // 컬렉션 추출
+            JSONObject jusoColl = (JSONObject) jusoArray.get(0);
+
+            // 행정동 코드
+            String h_code = jusoColl.get("admCd").toString();
+
+            // 지번 특수문자, 띄어쓰기 빼고 6자리로 만들기
+            String addName = jusoColl.get("jibunAddr").toString();
+            String match = "[^\uAC00-\uD7A30-9a-zA-Z\\s]";
+            addName = addName.replaceAll(match, "");                 // 특수문자 띄어쓰기 없애기
+            String intAdd1 = addName.replaceAll("[^0-9]", ""); // 지번만 추출
+            int intAdd2 = Integer.parseInt(intAdd1);                            // int 로 형변환
+
+            String jibun = String.format("%06d",intAdd2);                       // 최종 결과
+
+
+
+            System.out.println(h_code + jibun);
 
         } catch (Exception e) {
             e.printStackTrace();
